7d925826fc27a513f1212641e07a947c418606d8
Robin Tram <robin.tram@gmail.com>
2019-02-21 19:29:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function bfs(rootNode, vertices, edges){
     let adjacentVertices = findAdjacent(firstNode.name, vertices, edges)
     markDistanceAndPredecessor(firstNode, adjacentVertices)
     console.log(queue)
-    queue.concat(adjacentVertices) 
+    console.log(queue.concat(adjacentVertices) )
     console.log('queue', queue)
     // order.push(adjacentVertices)
   }
